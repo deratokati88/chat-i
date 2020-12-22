5f8d05cb104b63004518c3f3dc6fec6922167005
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-22 16:21:22 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
+  validates :name,:image, presence: true
 end
