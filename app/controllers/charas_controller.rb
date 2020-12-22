32d85a2eea638d3149ec4e595a62fd1ef1e84c0d
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-22 17:04:48 +0900

    キャラクリエイト実装

--- app/controllers/charas_controller.rb
+++ app/controllers/charas_controller.rb
@@ -3,5 +3,17 @@ class CharasController < ApplicationController
   @chara = Chara.new
   end
 
-  
+  def create
+    @chara = Chara.new(chara_params)
+    if @chara.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def chara_params
+    params.require(:chara).permit(:name,:image)
+  end
 end
