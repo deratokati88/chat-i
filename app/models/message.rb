46aa28d9d037f6d941eaafa69d3f8486e7fed7d0
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-24 12:47:13 +0900

    キャラ別、気持ち別にセリフを抜き出すようにした

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one :chara_message
+  has_one :message_content
 
   validates :content,:feeling_id, presence: true
 
@@ -14,7 +15,8 @@ class Message < ApplicationRecord
   def chara_messages_build
     @message = Message.last
     @room = Room.find(@message.room_id)
-    @content = MessageContent.find(1)
+    @content = ""
+    content_create
     @chara_message = CharaMessage.new
     @chara_message.content = @content.content
     @chara_message.feeling_id = @message.feeling_id
@@ -22,6 +24,11 @@ class Message < ApplicationRecord
     @chara_message.chara_id = @room.chara_id
     @chara_message.room_id = @room.id
     @chara_message.save
+  end
+
+  def content_create
+    target_id = MessageContent.where(chara_id: @room.chara_id).where(feeling_id: @message.feeling_id).pluck(:id).sample(1)
+    @content = MessageContent.find(target_id[0])
   end
 
 end
