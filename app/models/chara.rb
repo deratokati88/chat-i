8edc7ff1273194ee93cffe0d9867f9aece523314
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-25 12:10:03 +0900

    キャラテーブルにユーザーIDを追加

--- app/models/chara.rb
+++ app/models/chara.rb
@@ -2,6 +2,7 @@ class Chara < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   validates :name,:image, presence: true
+  belongs_to :user
   has_many :rooms
   has_many :chara_messages
   has_one :message_content
--- db/migrate/20201222073540_create_charas.rb
+++ db/migrate/20201222073540_create_charas.rb
@@ -3,6 +3,7 @@ class CreateCharas < ActiveRecord::Migration[6.0]
     create_table :charas do |t|
       t.string :name,    null: false
       t.string :image,   null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
