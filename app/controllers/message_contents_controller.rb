c18d4cda3eeb571f03fdcab6243782bd76aedb33
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-28 12:36:36 +0900

    キャラメッセージ作成画面実装

--- app/controllers/message_contents_controller.rb
+++ app/controllers/message_contents_controller.rb
@@ -1,5 +1,20 @@
 class MessageContentsController < ApplicationController
   def new
     @chara = Chara.find(params[:chara_id])
+    @message_content = MessageContent.new
+  end
+
+  def create
+    @chara = Chara.find(params[:chara_id])
+    @message_content = MessageContent.new(message_content_params)
+    if @message_content.save
+      redirect_to user_path(@chara.user_id)
+    else
+      render :new
+    end
+  end
+
+  def message_content_params
+    params.require(:message_content).permit(:feeling_id, :favo, :content).merge(chara_id: params[:chara_id])
   end
 end
